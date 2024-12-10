6d0acb56eb7d65a103d43e3ff62da1093efd9310
Marco Munizaga <git@marcopolo.io>
2024-12-10 09:14:06 -0800

    Revert "add debug conngater"
    This reverts commit 0f7f8852f6f9482ce773ef8dba0cd4a34c465ea4.

--- core/node/groups.go
+++ core/node/groups.go
@@ -154,7 +154,7 @@ func LibP2P(bcfg *BuildCfg, cfg *config.Config, userResourceOverrides rcmgr.Part
 		fx.Provide(libp2p.ResourceManager(bcfg.Repo.Path(), cfg.Swarm, userResourceOverrides)),
 		maybeProvide(libp2p.P2PForgeCertMgr(bcfg.Repo.Path(), cfg.AutoTLS), enableAutoTLS),
 		maybeInvoke(libp2p.StartP2PAutoTLS, enableAutoTLS),
-		// fx.Provide(libp2p.AddrFilters(cfg.Swarm.AddrFilters)),
+		fx.Provide(libp2p.AddrFilters(cfg.Swarm.AddrFilters)),
 		fx.Provide(libp2p.AddrsFactory(cfg.Addresses.Announce, cfg.Addresses.AppendAnnounce, cfg.Addresses.NoAnnounce)),
 		fx.Provide(libp2p.SmuxTransport(cfg.Swarm.Transports)),
 		fx.Provide(libp2p.RelayTransport(enableRelayTransport)),
